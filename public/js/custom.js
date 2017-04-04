079cba6b577275b0555e6170eb732ff2e46b6b21
d0ct0r4r6a <arga.saragih@gmail.com>
2017-04-04 22:35:18 +0700

    Copied static files to public dir

---
+++ public/js/custom.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('input[type=checkbox]').iCheck({
+    checkboxClass: 'icheckbox_square-green',
+    increaseArea: '20%'
+  });
+
+  $('#check-all').on('ifChecked', function(e){
+    $('.check-item').iCheck('check');
+  });
+  
+  $('#check-all').on('ifUnchecked', function(e){
+    $('.check-item').iCheck('uncheck');
+  });
+
+});
