c6ffb703a5a5ca9bc1065c98d8ab71d17a44c9a3
d0ct0r4r6a <arga.saragih@gmail.com>
2017-04-04 16:12:07 +0700

    Static prototype pt 2

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,15 @@
-$('.show-todolist-modal').click( function (event) {
-  event.preventDefault();
-  $('#todolist-modal').modal('show');
+$(function() {
+  $('input[type=checkbox]').iCheck({
+    checkboxClass: 'icheckbox_square-green',
+    increaseArea: '20%'
+  });
+
+  $('#check-all').on('ifChecked', function(e){
+    $('.check-item').iCheck('check');
+  });
+  
+  $('#check-all').on('ifUnchecked', function(e){
+    $('.check-item').iCheck('uncheck');
+  });
+
 });
